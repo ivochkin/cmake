aa89d861431b205d8d27c0234de13d1cdbe85d57
Stanislav Ivochkin <isn@extrn.org>
2016-12-02 16:35:41 +0300

    dfk, dfkcpp: Find via pkgconfig

--- Finddfk.cmake
+++ Finddfk.cmake
@@ -1,32 +1,36 @@
-find_path(DFK_INCLUDE_DIR dfk.h
-  PATH_SUFFIXES include
-  PATHS
-    /usr/local
-    /usr
-    /opt/local
-    /opt
-    /opt/isn
-    ${DFK_DIR})
+list(APPEND extra_search_paths /usr/local)
+list(APPEND extra_search_paths /usr)
+list(APPEND extra_search_paths /opt/local)
+list(APPEND extra_search_paths /opt)
+# packages from https://bintray.com/isn/deb are installed into /opt/isn
+list(APPEND extra_search_paths /opt/isn)
+list(APPEND extra_search_paths "${DFK_DIR}")
 
-find_library(DFK_LIBRARY
-  NAMES
-    dfk
-    libdfk
-  PATH_SUFFIXES
-    lib64
-    lib
-  PATHS
-    /usr/local
-    /usr
-    /opt/local
-    /opt
-    /opt/isn
-    ${DFK_DIR})
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+foreach(path ${extra_search_paths})
+  list(APPEND CMAKE_PREFIX_PATH ${path})
+endforeach()
+
+find_package(PkgConfig)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(DFK QUIET dfk)
+endif()
+
+if(NOT DFK_FOUND)
+  find_path(DFK_INCLUDE_DIRS dfk.hpp
+    PATH_SUFFIXES include
+    PATHS ${extra_search_paths})
+
+  find_library(DFK_LIBRARIES
+    NAMES
+      dfk
+      libdfk
+    PATH_SUFFIXES
+      lib64
+      lib
+    PATHS ${extra_search_paths})
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DFK REQUIRED_VARS DFK_INCLUDE_DIR DFK_LIBRARY)
-mark_as_advanced(DFK_INCLUDE_DIR DFK_LIBRARY)
-if(DFK_FOUND)
-  set(DFK_LIBRARIES ${DFK_LIBRARY})
-  set(DFK_INCLUDE_DIRS ${DFK_INCLUDE_DIR})
-endif()
+find_package_handle_standard_args(DFK REQUIRED_VARS DFK_INCLUDE_DIRS DFK_LIBRARIES)
+mark_as_advanced(DFK_INCLUDE_DIRS DFK_LIBRARIES)
--- Finddfkcpp.cmake
+++ Finddfkcpp.cmake
@@ -1,32 +1,36 @@
-find_path(DFKCPP_INCLUDE_DIR dfk.hpp
-  PATH_SUFFIXES include
-  PATHS
-    /usr/local
-    /usr
-    /opt/local
-    /opt
-    /opt/isn
-    ${DFKCPP_DIR})
+list(APPEND extra_search_paths /usr/local)
+list(APPEND extra_search_paths /usr)
+list(APPEND extra_search_paths /opt/local)
+list(APPEND extra_search_paths /opt)
+# packages from https://bintray.com/isn/deb are installed into /opt/isn
+list(APPEND extra_search_paths /opt/isn)
+list(APPEND extra_search_paths "${DFKCPP_DIR}")
 
-find_library(DFKCPP_LIBRARY
-  NAMES
-    dfkcpp
-    libdfkcpp
-  PATH_SUFFIXES
-    lib64
-    lib
-  PATHS
-    /usr/local
-    /usr
-    /opt/local
-    /opt
-    /opt/isn
-    ${DFKCPP_DIR})
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+foreach(path ${extra_search_paths})
+  list(APPEND CMAKE_PREFIX_PATH ${path})
+endforeach()
+
+find_package(PkgConfig)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(DFKCPP QUIET dfkcpp)
+endif()
+
+if(NOT DFKCPP_FOUND)
+  find_path(DFKCPP_INCLUDE_DIRS dfk.hpp
+    PATH_SUFFIXES include
+    PATHS ${extra_search_paths})
+
+  find_library(DFKCPP_LIBRARIES
+    NAMES
+      dfkcpp
+      libdfkcpp
+    PATH_SUFFIXES
+      lib64
+      lib
+    PATHS ${extra_search_paths})
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DFKCPP REQUIRED_VARS DFKCPP_INCLUDE_DIR DFKCPP_LIBRARY)
-mark_as_advanced(DFKCPP_INCLUDE_DIR DFKCPP_LIBRARY)
-if(DFKCPP_FOUND)
-  set(DFKCPP_LIBRARIES ${DFKCPP_LIBRARY})
-  set(DFKCPP_INCLUDE_DIRS ${DFKCPP_INCLUDE_DIR})
-endif()
+find_package_handle_standard_args(DFKCPP REQUIRED_VARS DFKCPP_INCLUDE_DIRS DFKCPP_LIBRARIES)
+mark_as_advanced(DFKCPP_INCLUDE_DIRS DFKCPP_LIBRARIES)
