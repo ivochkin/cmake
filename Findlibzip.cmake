5f89eb86a7e1187e997b3320293fed5cb6f33f0b
Stanislav Ivochkin <isn@extrn.org>
2017-12-16 20:35:35 +0300

    Add find moduel for libzip

---
+++ Findlibzip.cmake
@@ -0,0 +1,36 @@
+# Finds libzip.
+#
+# This module defines:
+# LIBZIP_INCLUDE_DIR
+# LIBZIP_LIBRARIES
+#
+
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBZIP QUIET libzip)
+
+find_path(LIBZIP_INCLUDE_DIR
+    NAMES zip.h
+    HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+
+find_path(LIBZIP_INCLUDE_DIR_ZIPCONF
+    NAMES zipconf.h
+    HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+
+find_library(LIBZIP_LIBRARIES
+    NAMES zip)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    LIBZIP DEFAULT_MSG
+    LIBZIP_LIBRARIES LIBZIP_INCLUDE_DIR LIBZIP_INCLUDE_DIR_ZIPCONF)
+
+set(LIBZIP_VERSION 0)
+
+if (LIBZIP_INCLUDE_DIR_ZIPCONF)
+  FILE(READ "${LIBZIP_INCLUDE_DIR_ZIPCONF}/zipconf.h" _LIBZIP_VERSION_CONTENTS)
+  if (_LIBZIP_VERSION_CONTENTS)
+    STRING(REGEX REPLACE ".*#define LIBZIP_VERSION \"([0-9.]+)\".*" "\\1" LIBZIP_VERSION "${_LIBZIP_VERSION_CONTENTS}")
+  endif ()
+endif ()
+
+set(LIBZIP_VERSION ${LIBZIP_VERSION} CACHE STRING "Version number of libzip")
